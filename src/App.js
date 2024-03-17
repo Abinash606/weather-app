f49e9aff6541a2260609a1f674fef04f55cb8427
E.D.I.T.H <abinashsahoo4063@gmail.com>
2024-03-17 21:30:04 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import WeatherApp from './Componments/WeatherApp/WeatherApp';
+
+function App() {
+  return (
+    <div className='App'>
+      <WeatherApp/>
+    </div>
+  );
+}
+
+export default App;
